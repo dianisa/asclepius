6265871707fde08a3a17b492ebb6a5ef9da41702
Dianisa Wulandari <dianisa.w@agiletechnica.com>
2024-05-22 21:03:40 +0700

    change confidenceScore threshold

--- src/server/handler.js
+++ src/server/handler.js
@@ -18,7 +18,7 @@ export const postPredictHandler = async (request, h) => {
 
     let response;
 
-    if (confidenceScore > 99) {
+    if (confidenceScore > 60) {
         await storeData(id, data);
 
         response = h.response({
